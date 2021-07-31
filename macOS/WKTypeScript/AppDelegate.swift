a17b2960f88bf20d8d84065481e20cfc0e60ba88
Justin Bush <revblaze@users.noreply.github.com>
2021-07-31 12:57:55 -0400

    (macOS) Create Xcode project

---
+++ macOS/WKTypeScript/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WKTypeScript
+//
+//  Created by Justin Bush on 2021-07-31.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
