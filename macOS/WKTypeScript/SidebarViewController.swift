6b5c11d88fea68c32ae3dfe601575cae2ea7c5fb
Justin Bush <revblaze@users.noreply.github.com>
2021-08-04 12:04:35 -0400

    Create SidebarViewController.swift

---
+++ macOS/WKTypeScript/SidebarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SidebarViewController.swift
+//  WKTypeScript
+//
+//  Created by Justin Bush on 2021-08-04.
+//
+
+import Cocoa
+
+class SidebarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
