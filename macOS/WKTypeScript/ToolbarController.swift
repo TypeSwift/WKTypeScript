8518ccb646d6b8a7ea04610ff4c59d7250178d9c
Justin Bush <revblaze@users.noreply.github.com>
2021-08-05 09:46:41 -0400

    Setup SidebarMenu

---
+++ macOS/WKTypeScript/ToolbarController.swift
@@ -0,0 +1,19 @@
+//
+//  ToolbarController.swift
+//  WKTypeScript
+//
+//  Created by Justin Bush on 2021-08-05.
+//
+
+import Cocoa
+
+class Toolbar: NSToolbar, NSToolbarDelegate { }
+
+class ToolbarController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ macOS/WKTypeScript/WindowController.swift
@@ -0,0 +1,18 @@
+//
+//  WindowController.swift
+//  WKTypeScript
+//
+//  Created by Justin Bush on 2021-08-05.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController, NSWindowDelegate, NSToolbarDelegate {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
